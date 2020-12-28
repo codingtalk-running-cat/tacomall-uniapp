3d7638784f1b0d5700c91bf151924548ac7c8080
Bertha-zxd <1258132495@qq.com>
2020-12-28 23:53:27 +0800

    1，seckill静态页

--- pages/seckill/index.vue
+++ pages/seckill/index.vue
@@ -10,5 +10,335 @@
  * @oops: Just do what I think it is right
 -->
 <template>
-    <view class="seckill"></view>
+    <view class="page seckill">
+      <view class="s-content">
+        <view class="c-backgrund">
+          <image src="../../static/image/seckill-background.png" />
+        </view>
+        <scroll-view class="c-wrapper">
+          <view class="w-half">
+            <view class="h-header">
+              <image src="../../static/image/seckill-half-logo.png" />
+              <span>官方精选 限量五折</span>
+            </view>
+            <view class="h-goods">
+              <scroll-view class="g-scrollview" scroll-x="true">
+                <view class="s-wrapper">
+                  <view class="w-item">
+                  <view class="i-top">
+                    <view class="t-time">
+                      <text>明日9点开抢</text>
+                    </view>
+                    <!-- <view class="t-time">
+                      <text>距结束2:30:49</text>
+                    </view> -->
+                    <view class="t-image">
+                      <image src="../../static/image/seckill-half-goods.png" />
+                    </view>
+                    <view class="t-status">
+                      <text>限量50件5折</text>
+                    </view>
+                    <!-- <view class="t-status">
+                      <text>正在秒杀</text>
+                    </view>
+                    <view class="t-status">
+                      <text>5折已抢光 恢复秒杀价</text>
+                    </view> -->
+                  </view>
+                  <view class="i-bottom">
+                    <view class="b-title">
+                      <text>即饮饮水机水吧</text>
+                    </view>
+                    <view class="b-info">
+                      <image src="../../static/image/seckill-half-button.png" />
+                      <view class="b-left">
+                        <text class="l-discount">￥199.5</text>
+                        <text class="l-original">￥399</text>
+                      </view>
+                      <view class="b-right">
+                        <view class="r-status">
+                          <text>提醒我</text>
+                          <text>53人预约</text>
+                        </view>
+                        <!-- <view class="r-status">
+                          <text>抢购</text>
+                        </view> -->
+                      </view>
+                    </view>
+                  </view>
+                  
+                  </view>
+                  <view class="w-item">
+                    <view class="i-top">
+                      <view class="t-time">
+                        <text>明日9点开抢</text>
+                      </view>
+                      <!-- <view class="t-time">
+                        <text>距结束2:30:49</text>
+                      </view> -->
+                      <view class="t-image">
+                        <image src="../../static/image/seckill-half-goods.png" />
+                      </view>
+                      <view class="t-status">
+                        <text>限量50件5折</text>
+                      </view>
+                      <!-- <view class="t-status">
+                        <text>正在秒杀</text>
+                      </view>
+                      <view class="t-status">
+                        <text>5折已抢光 恢复秒杀价</text>
+                      </view> -->
+                    </view>
+                    <view class="i-bottom">
+                      <view class="b-title">
+                        <text>即饮饮水机水吧</text>
+                      </view>
+                      <view class="b-info">
+                        <image src="../../static/image/seckill-half-button.png" />
+                        <view class="b-left">
+                          <text class="l-discount">￥199.5</text>
+                          <text class="l-original">￥399</text>
+                        </view>
+                        <view class="b-right">
+                          <view class="r-status">
+                            <text>提醒我</text>
+                            <text>53人预约</text>
+                          </view>
+                          <!-- <view class="r-status">
+                            <text>抢购</text>
+                          </view> -->
+                        </view>
+                      </view>
+                    </view> 
+                  </view>
+                  <view class="w-item">
+                    <view class="i-top">
+                      <view class="t-time">
+                        <text>明日9点开抢</text>
+                      </view>
+                      <!-- <view class="t-time">
+                        <text>距结束2:30:49</text>
+                      </view> -->
+                      <view class="t-image">
+                        <image src="../../static/image/seckill-half-goods.png" />
+                      </view>
+                      <view class="t-status">
+                        <text>限量50件5折</text>
+                      </view>
+                      <!-- <view class="t-status">
+                        <text>正在秒杀</text>
+                      </view>
+                      <view class="t-status">
+                        <text>5折已抢光 恢复秒杀价</text>
+                      </view> -->
+                    </view>
+                    <view class="i-bottom">
+                      <view class="b-title">
+                        <text>即饮饮水机水吧</text>
+                      </view>
+                      <view class="b-info">
+                        <image src="../../static/image/seckill-half-button.png" />
+                        <view class="b-left">
+                          <text class="l-discount">￥199.5</text>
+                          <text class="l-original">￥399</text>
+                        </view>
+                        <view class="b-right">
+                          <view class="r-status">
+                            <text>提醒我</text>
+                            <text>53人预约</text>
+                          </view>
+                          <!-- <view class="r-status">
+                            <text>抢购</text>
+                          </view> -->
+                        </view>
+                      </view>
+                    </view> 
+                  </view>
+                  <view class="w-item">
+                    <view class="i-top">
+                      <view class="t-time">
+                        <text>明日9点开抢</text>
+                      </view>
+                      <!-- <view class="t-time">
+                        <text>距结束2:30:49</text>
+                      </view> -->
+                      <view class="t-image">
+                        <image src="../../static/image/seckill-half-goods.png" />
+                      </view>
+                      <view class="t-status">
+                        <text>限量50件5折</text>
+                      </view>
+                      <!-- <view class="t-status">
+                        <text>正在秒杀</text>
+                      </view>
+                      <view class="t-status">
+                        <text>5折已抢光 恢复秒杀价</text>
+                      </view> -->
+                    </view>
+                    <view class="i-bottom">
+                      <view class="b-title">
+                        <text>即饮饮水机水吧</text>
+                      </view>
+                      <view class="b-info">
+                        <image src="../../static/image/seckill-half-button.png" />
+                        <view class="b-left">
+                          <text class="l-discount">￥199.5</text>
+                          <text class="l-original">￥399</text>
+                        </view>
+                        <view class="b-right">
+                          <view class="r-status">
+                            <text>提醒我</text>
+                            <text>53人预约</text>
+                          </view>
+                          <!-- <view class="r-status">
+                            <text>抢购</text>
+                          </view> -->
+                        </view>
+                      </view>
+                    </view> 
+                  </view>
+                  <view class="w-item">
+                    <view class="i-top">
+                      <view class="t-time">
+                        <text>明日9点开抢</text>
+                      </view>
+                      <!-- <view class="t-time">
+                        <text>距结束2:30:49</text>
+                      </view> -->
+                      <view class="t-image">
+                        <image src="../../static/image/seckill-half-goods.png" />
+                      </view>
+                      <view class="t-status">
+                        <text>限量50件5折</text>
+                      </view>
+                      <!-- <view class="t-status">
+                        <text>正在秒杀</text>
+                      </view>
+                      <view class="t-status">
+                        <text>5折已抢光 恢复秒杀价</text>
+                      </view> -->
+                    </view>
+                    <view class="i-bottom">
+                      <view class="b-title">
+                        <text>即饮饮水机水吧</text>
+                      </view>
+                      <view class="b-info">
+                        <image src="../../static/image/seckill-half-button.png" />
+                        <view class="b-left">
+                          <text class="l-discount">￥199.5</text>
+                          <text class="l-original">￥399</text>
+                        </view>
+                        <view class="b-right">
+                          <view class="r-status">
+                            <text>提醒我</text>
+                            <text>53人预约</text>
+                          </view>
+                          <!-- <view class="r-status">
+                            <text>抢购</text>
+                          </view> -->
+                        </view>
+                      </view>
+                    </view> 
+                  </view>
+                </view>
+              </scroll-view>
+            </view>
+          </view>
+          <view class="w-timeInterval">
+            <view class="t-left">
+              <view class="l-logo">
+                <text>限时热抢</text>
+              </view>
+              <view class="l-buying">
+                <text class="b-time">14:00</text>
+                <text class="b-text">抢购中</text>
+              </view>
+            </view>
+            <scroll-view class="t-right" scroll-x="true">
+              <view class="r-wrapper">
+                <view class="w-item">
+                  <text class="i-time">20:00</text>
+                  <text class="i-text">即将开始</text>
+                </view>
+                <view class="w-item">
+                  <text class="i-time">00:00</text>
+                  <text class="i-text">明日开始</text>
+                </view>
+                <view class="w-item">
+                  <text class="i-time">09:00</text>
+                  <text class="i-text">明日开始</text>
+                </view>
+                <view class="w-item">
+                  <text class="i-time">12:00</text>
+                  <text class="i-text">明日开始</text>
+                </view>
+                <view class="w-item">
+                  <text class="i-time">14:00</text>
+                  <text class="i-text">明日开始</text>
+                </view>
+              </view>
+            </scroll-view>
+          </view>
+          <scroll-view class="w-hotgrabList" scroll-y="true">
+            <view class="h-wrapper">
+              <view class="w-image">
+                <image src="../../static/image/seckill-hotgrabList-top.png"/>
+              </view>
+              <view class="w-item">
+                <view class="i-left">
+                  <image class="l-image" src="../../static/image/seckill-hotgrabList-good.jpg"/>
+                  <view class="l-label">
+                    <image src="../../static/image/seckill-recommend-label.png"/>
+                  </view>
+                </view>
+                <view class="i-right">
+                  <view class="r-title">
+                    <text>丽得姿面膜</text>
+                  </view>
+                  <view class="r-desc">
+                    <text>打破皮肤“免疫”局面，17种氨基酸成分，渗入润护</text>
+                  </view>
+                  <view class="r-progress">
+                    <text>已抢15%</text>
+                  </view>
+                  <view class="r-shopping">
+                    <view class="s-lower">
+                      <text>365天历史最低价</text>
+                    </view>
+                    <view class="s-price">
+                      <text class="p-yuan">¥</text>
+                      <text class="p-lowerPrice">89</text>
+                      <text class="p-text">起</text>
+                      <text class="p-originalPrice">¥367</text>
+                    </view>
+                    <view class="s-button">
+                      <text>立即抢</text>
+                    </view>
+                  </view>
+                </view>
+              </view>
+            </view>
+          </scroll-view>
+        </scroll-view>
+      </view>
+
+    </view>
 </template>
+<script>
+export default {
+  methods: {
+    init() {
+      uni.setNavigationBarColor({
+        frontColor: '#ffffff',
+        backgroundColor: '#FF3312'
+      })
+    },
+  },
+  onLoad() {
+    this.init()
+  }
+}
+</script>
+<style lang="less" scoped>
+@import './index';
+</style>
